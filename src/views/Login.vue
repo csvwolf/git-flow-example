16918c8cc33c43a688f54d4e4a8c54b232ab0e82
SkyAo <csvwolf@qq.com>
2021-03-15 22:56:51 +0800

    feat: login page

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+import { GetUser } from '@/store/user'
+export default {
+  name: 'User.vue',
+  methods: {
+    getUser () {
+      return GetUser
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
